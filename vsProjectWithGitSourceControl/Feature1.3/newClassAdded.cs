3f5e4370305c5bcaf530f43edab70ea00b920923
Laith Maali <laithmaali2007@gmail.com>
2014-04-02 14:49:13 +0300

    new file added

---
+++ vsProjectWithGitSourceControl/Feature1.3/newClassAdded.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vsProjectWithGitSourceControl.Feature1.3
+{
+	public class newClassAdded
+	{
+	}
+}
